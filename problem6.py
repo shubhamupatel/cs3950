905814a54705c2680c5957158a334a5611dcaa9f
Shubham Patel <shubhamupatel7@gmail.com>
2026-06-12 01:45:57 -0700

    Create problem6.py

---
+++ problem6.py
@@ -0,0 +1,21 @@
+def problem6(source, target):
+    m, n = len(source), len(target)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if source[i-1] == target[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(
+                    dp[i-1][j],      # delete
+                    dp[i][j-1],      # insert
+                    dp[i-1][j-1]     # replace
+                )
+
+    return dp[m][n]
